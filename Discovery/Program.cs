a97033fd67fe971ed99ee7eb5747551726900752
GALLB0603 <bruno.galliez@chwapi.be>
2022-08-19 16:10:21 +0200

    end of work

--- Discovery/Program.cs
+++ Discovery/Program.cs
@@ -1,7 +1,13 @@
+using Discovery.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Accès aux données directement dans les service de l'app
+builder.Services.AddDbContext<AspNetIdentityDbContext>(opts => opts.UseSqlite(builder.Configuration["DbConnectionString"]));
 
 var app = builder.Build();
 
@@ -21,3 +27,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
