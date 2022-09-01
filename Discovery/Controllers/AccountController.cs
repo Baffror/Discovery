c5f9b1d4a2b6711b6d4c2300048b71e38baf2d55
Baffror <galliez.bruno@gmail.com>
2022-09-01 13:30:34 +0200

    Creation de compte

--- Discovery/Controllers/AccountController.cs
+++ Discovery/Controllers/AccountController.cs
@@ -1,10 +1,22 @@
-﻿using Discovery.Models;
+﻿using Discovery.Data.Models;
+using Discovery.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Discovery.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<User> signInManager;
+
+        private readonly UserManager<User> UserManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.UserManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -12,9 +24,36 @@ namespace Discovery.Controllers
             return View(vm);
         }
     
-        public async Task<IActionResult> CreateAccount()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            return View();  
+            if (ModelState.IsValid)
+            {
+
+                if (model.Password == model.ConfirmPassword) { 
+
+                var user = new User
+                {
+                    Email = model.Email,
+                    Name = model.Name,
+                    Lastname = model.Lastname,
+                    UserName = model.Email
+                };
+                var result = await UserManager.CreateAsync(user,model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false); 
+                    return RedirectToAction("Index", "Home");
+                }
+                } else
+                {
+                    ModelState.AddModelError("incorrect_confirmation","Les mots de passes ne conrrespondent pas");
+                }
+            }
+            model.Password = String.Empty;
+            model.ConfirmPassword = String.Empty;
+            return View(model);  
         }
 
     }
