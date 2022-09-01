990c1d4d7ec526b3b03e311b8a6c80db477db91a
Baffror <galliez.bruno@gmail.com>
2022-09-01 10:49:54 +0200

    Create account - Model  & View

---
+++ Discovery/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Discovery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discovery.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var vm = new RegisterViewModel();
+            return View(vm);
+        }
+    
+        public async Task<IActionResult> CreateAccount()
+        {
+            return View();  
+        }
+
+    }
+}
---
+++ Discovery/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Discovery.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string  Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmation du mot de passe")]
+        public string  ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "Nom")]
+        public string  Name { get; set; }
+        [Required]
+        [Display(Name = "Prénom")]
+        public string  Lastname { get; set; }
+    }
+}
